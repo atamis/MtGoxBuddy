0d5126e8dd7a78cde6458457ea0bb65ba32f391e
Indigo Casson <atamiser@gmail.com>
2011-07-27 13:20:31 -0700

    You can now specify the wait inverval from the command line.
    Updated to be unreadable, on one line, in ternary.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy, time, json, urllib
+import numpy, time, json, urllib, sys
 def check():
     data = json.loads(urllib.urlopen('https://mtgox.com/code/data/ticker.php').read())['ticker']
     return (data['low'],data['high'],data['last'])
@@ -8,4 +8,4 @@ def checkPriceRange(prices):
     if prices[-1] > numpy.average(prices)+numpy.std(prices):print "Price increasing", prices[-1], rangeText #if last price is greater than average + standard deviation, it's irregularly high
     if prices[-1] < numpy.average(prices)-numpy.std(prices):print "Price decreasing", prices[-1], rangeText #if last price is lower than average - standard deviation, it's irregularly low
     else: print "Price in normal range", prices[-1], rangeText
-while 1: last=check()[2]; lasts.append(last);checkPriceRange(lasts[-10:]);time.sleep(5)
+while 1: last=check()[2]; lasts.append(last);checkPriceRange(lasts[-10:]);time.sleep(float(sys.argv[1]) if len(sys.argv) > 1 else 60)
